c374db4164ca75ad4dd604357534978aa29561c0
Naiim Taefy Jimenez <naiim.tj@gmail.com>
2024-08-03 17:10:48 +0200

    feat: services-update dependence and add frameworks

--- services/src/components/PostList.vue
+++ services/src/components/PostList.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>LISTADO DE POST</h1>
-    <ul class="post-list">
+    <ul class="post-list"  >
         <li v-for="post in posts" :key="post.id">{{post.title}}</li>
     </ul>
 </template >
@@ -36,7 +36,6 @@ export default defineComponent({
 </script> -->
 
 <style scoped lang="scss">
-$red: #ff0000;
 .post-list{
   width: 95vw;
   height: 75px;
