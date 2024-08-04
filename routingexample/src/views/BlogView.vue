9b9f81c2a246911b20cbd468067c9b539da1528d
Naiim Taefy Jimenez <naiim.tj@gmail.com>
2024-08-04 22:06:58 +0200

    add: routing

---
+++ routingexample/src/views/BlogView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>BLOG SECTION</h1>
+  <ul>
+    <li>
+      <router-link :to="{name: 'post', params:{id:1}}">Primer Post</router-link>
+    </li>
+    <li>
+      <router-link :to="{name: 'post', params:{id:2}}">Segundo Post</router-link>
+    </li>
+    <li>
+      <router-link :to="{name: 'post', params:{id:3}}">Tercero Post</router-link>
+    </li>
+    <li>
+      <router-link :to="{name: 'post', params:{id:4}}">Cuarto Post</router-link>
+    </li>
+  </ul>
+</template>
